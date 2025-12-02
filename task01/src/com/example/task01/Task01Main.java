e6e27dd9ef784e6f63099c67e60bb40a97d0bafc
Mishbringer <vip.kukov@bk.ru>
2025-12-03 02:31:51 +0500

    task01 solve

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -1,6 +1,6 @@
 package com.example.task01;
 
-import java.io.File;
+import java.io.*;
 import java.io.IOException;
 
 public class Task01Main {
@@ -8,13 +8,27 @@ public class Task01Main {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
 
-        /*
         System.out.println(extractSoundName(new File("task01/src/main/resources/3727.mp3")));
-        */
+
     }
 
     public static String extractSoundName(File file) throws IOException, InterruptedException {
-        // your implementation here
-        return "sound name";
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command("ffprobe", "-v", "error", "-of", "flat", "-show_format", file.getAbsolutePath());
+
+        Process process = processBuilder.start();
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.contains("format.tags.title")) {
+                    int start = line.indexOf('"');
+                    int end = line.lastIndexOf('"');
+                    if (start != -1 && end != -1) return line.substring(start + 1, end);
+                }
+            }
+        }
+
+        return null;
     }
 }
